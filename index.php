22f7fdd27d60ad4597209d4e8e7e5dfe08ac8e5f
Malena <malena10vinas@gmail.com>
2021-10-21 14:30:01 +0200

    1º versión estilo

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <html>
     <head>
-        <title>CRUD</title>
+        <title>Escape Web</title>
         <script src="Script/validacion.js"></script>
+        <link rel="stylesheet" href="./CSS/general.css">
     </head>
+
     <body>
         <?php
             require_once './BBDD/conexion.php';
@@ -12,18 +14,25 @@
                 unset($_SESSION["mensajeError"]);
             }
         ?>
+        <div class="container">
+            <header class="row">
+                <div class="col-e-4 col-m-12">
+                <img src="" alt="">
+                </div>
+            </header>
         
-        <form action="./Controlador/registro_IS.php" method="post">
-            <label for="mail">
-                <span>E-mail:</span>
-                <input type="email" name="correo" id="mail" required minlength="5">
-                <span class="error" aria-live="polite"></span>
-            </label> <br><br>
-            
-            Contraseña  <input type="password" name="contraseña" value="" required minlength="5"><br><br>
-            <input type="submit" value="Iniciar sesión" name="iniciarSesion">
-        </form>
-        <a href="registro.php">Registrarse</a>      
-        <a href="olvideContraseña.php">He olvidado la contraseña</a>
+            <form action="./Controlador/registro_IS.php" method="post">
+                <label for="mail">
+                    <span>E-mail:</span>
+                    <input type="email" name="correo" id="mail" required minlength="5">
+                    <span class="error" aria-live="polite"></span>
+                </label> <br><br>
+                
+                Contraseña  <input type="password" name="contraseña" value="" required minlength="5"><br><br>
+                <input type="submit" value="Iniciar sesión" name="iniciarSesion">
+            </form>
+            <a href="registro.php">Registrarse</a>      
+            <a href="olvideContraseña.php">He olvidado la contraseña</a>
+        </div>        
     </body>
 </html>
